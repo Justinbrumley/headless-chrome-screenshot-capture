c39c4d6e52fe4484a904ba615d9f12c5ce8a3f9a
Justin Brumley <justin@appsumo.com>
2017-06-21 08:02:03 -0500

    If renderable is set to false, loop until it is true.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const CDP = require('chrome-remote-interface');
 async function launchChrome(headless = true) {
   return await chromeLauncher.launch({
     port: 9222,
-    flags: [
+    chromeFlags: [
       '--window-size=412,732',
       '--disable-gpu',
       headless ? '--headless' : ''
@@ -19,18 +19,46 @@ async function launchChrome(headless = true) {
   const { Page, Runtime } = protocol;
   await Promise.all([ Page.enable(), Runtime.enable() ]);
 
+  console.log('Opening url...');
   Page.navigate({ url: 'http://dev.sumo.com/apps/listbuilder/v3/template/06977c14-2f9c-41b9-831c-5b6992ca397f/phantom' });
 
   Page.loadEventFired(async () => {
+    console.log('Load event fired...');
     async function evaluate() {
-      const result = await Runtime.evaluate({ expressions: 'window.renderable' });
+      const result = await Runtime.evaluate({ expression: 'window.renderable' });
       return result.result.value;
     }
 
-    const renderable = await evaluate('window.renderable');
+    async function loop() {
+      let renderable;
+      try {
+        renderable = await evaluate('window.renderable');
+      } catch(e) {
+        console.log('Error checking renderable value:', e);
+      }
 
-    console.log('Page renderable?', renderable);
-    protocol.close();
-    chrome.kill();
+      console.log('Renderable:', renderable);
+
+      // Save screenshot if renderable is true or renderable isn't set
+      if (renderable === undefined || renderable) {
+        let image;
+        try {
+          image = await Page.captureScreenshot();
+        } catch(e) {
+          console.log('Error capturing screenshot:', e);
+        }
+
+        console.log('Image:', image);
+
+        protocol.close();
+        chrome.kill();
+      } else {
+        setTimeout(async () => {
+          await loop();
+        }, 500);
+      }
+    }
+
+    await loop();
   });
 })();
